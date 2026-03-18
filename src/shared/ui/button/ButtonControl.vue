91f2b5f12cf2dc08b3904635f1d19a85691ce592
MarinaLagarnikova <m.lagarnikova@dvizh.io>
2026-03-18 20:02:21 +0300

    feat: add Button component

---
+++ src/shared/ui/button/ButtonControl.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Button } from '@headlessui/vue'
+import { computed } from 'vue'
+import TouchTarget from './TouchTarget.vue'
+import { buttonStyles } from './styles'
+import type { ButtonColor, ButtonControlProps } from './types'
+import { clsx } from '@/shared/lib'
+
+const props = withDefaults(defineProps<ButtonControlProps>(), {
+  color: 'dark/zinc',
+  outline: false,
+  plain: false,
+  disabled: false,
+  type: 'button',
+})
+
+const emit = defineEmits<{
+  click: []
+}>()
+
+const handleClick = () => {
+  if (!props.disabled) {
+    emit('click')
+  }
+}
+
+const computedClasses = computed(() => {
+  return clsx(
+    props.class,
+    buttonStyles.base,
+    props.outline
+      ? buttonStyles.outline
+      : props.plain
+        ? buttonStyles.plain
+        : [...buttonStyles.solid, ...(buttonStyles.colors[props.color as ButtonColor] || buttonStyles.colors['dark/zinc'])],
+    props.disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer',
+  )
+})
+</script>
+
+<template>
+  <Button
+    :type="type"
+    :disabled="disabled"
+    :class="computedClasses"
+    @click="handleClick"
+  >
+    <TouchTarget>
+      <slot />
+    </TouchTarget>
+  </Button>
+</template>
