c27f70c26f12c239f249ae27747305e3c5da197c
MarinaLagarnikova <m.lagarnikova@dvizh.io>
2026-03-18 17:08:49 +0300

    chore: install and configure Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  "docs": {
+    "autodocs": "tag",
+  },
+  "framework": "@storybook/vue3-vite"
+};
+export default config;
